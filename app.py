941327b76937830648a1bdbc16939d991cf1634c
Lamox <zklein1992@gmai.com>
2024-06-18 13:49:55 +0100

    Flask_app_first

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-#empty
+from flask import Flask
+from views import my_view
+
+app = Flask(__name__)
+app.register_blueprint(my_view)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
